3bca9ed7d92af81cbedaf1dc887410a18e24b8fc
quarknetUPENN <quarknetpenn@gmail.com>
2015-06-29 16:28:55 -0400

    more changes
    working on correlation

--- Python/StatisticalAnalysis.py
+++ Python/StatisticalAnalysis.py
@@ -3,10 +3,12 @@ from numpy import mean
 from numpy import linspace
 import matplotlib.pyplot as plt
 
-#test arrays
+#Regression Test Arrays
+
 x = [12.0, 8.0, 4.0, 0.0]
 y = [4.0, 2.0, 4.0, 2.0]
 r = [1.2, 1, 0.8, 0.9]
+xbar = mean(x)
 ybar = mean(y)
 
 y_adj = []
@@ -14,39 +16,70 @@ xy = []
 xx = []
 n = len(x)
 computationTime = 0
+correlation = 0
+ssxx = []
+ssyy = []
+ssxy = []
 
-t0 = time.clock()
 
-for i in range(0,n):
+
+#Regression Computation
+
+t0 = time.clock() #starts timer
+
+for i in range(0,n): #creates y_adj
     if (y[i] < ybar):
         y_adj.append(y[i] + r[i])
     else:
         y_adj.append(y[i] - r[i])
-for i in range(0,n):
+for i in range(0,n): #creates xy
     xy.append(x[i]*y_adj[i])
-for i in range(0,n):
+for i in range(0,n): #creates xx
     xx.append(x[i]**2)
     
+sumxx = sum(xx)
+sumxy = sum(xy)
     
+a = ((ybar*sumxx - xbar*sumxy)) / (sumxx-n*(xbar**2)) #finds a
+b = (sumxy - n*xbar*ybar)/(sumxx-n*(xbar**2)) #finds b
+
+t1 = time.clock() #ends timer
+
+computationTime = t1 - t0 #calculates time
+
+
+
+
+# Regression Analysis         
+
+for i in range(0,n): #creates ssxx
+    ssxx.append((x[i]-xbar)**2)
+for i in range(0,n): #creates ssyy
+    ssyy.append((y[i]-ybar)**2)
+for i in range(0,n): #creates ssxy
+    ssxy.append(((y[i]-ybar)**2)*((x[i]-xbar)**2)) 
     
-a = ((mean(y)*(sum(xx)) - mean(x)*sum(xy))) / (sum(xx)-n*(mean(x)**2))
-b = (sum(xy) - n*mean(x)*mean(y))/(sum(xx)-n*(mean(x)**2))
+ssxx = sum(ssxx)
+ssyy = sum(ssyy)
+ssxy = sum(ssxy)
 
-t1 = time.clock()
+correlation = (ssxy**2)/(ssxx*ssyy)
 
-computationTime = t1 - t0
+
+
+#Regression Display
 
 print 'x: ' + str(x)
 print 'y: ' + str(y)
 print 'r: ' + str(r)
 print 'y_adj: ' + str(y_adj)
-print 'xx: ' + str(xx)
-print 'xy: ' + str(xy)
-print 'n: ' + str(n)
-    
+print 'xbar: ' + str(xbar)
+print 'ybar: ' + str(ybar)
+
 print "a: " + str(a)
 print "b: " + str(b)
-print "Capital R Squared: " + str(computationTime)
+print "Correlation: " + str(correlation)
+print "ComputationTime: " + str(computationTime)
 
 plt.scatter(x,y)
 axis = linspace(min(x),max(x),100)
