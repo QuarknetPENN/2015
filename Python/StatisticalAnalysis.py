190001a0951a3b1153537382ad626ecee7e12773
quarknetUPENN <quarknetpenn@gmail.com>
2015-06-30 16:11:29 -0400

    Cleaned up data and improved accuracy of regression

--- Python/StatisticalAnalysis.py
+++ Python/StatisticalAnalysis.py
@@ -1,106 +1,100 @@
 import time
 from numpy import mean
 from numpy import linspace
-from numpy import pi
+from scipy import stats
 import matplotlib.pyplot as plt
 
 
-def analysis(xin,yin,rin):
+def analysis(xin,yin,rin,plot):
     '''
     '''
     
+    t0 = time.clock() #starts timer
     #Regression Test Arrays
     x = list(xin)
     y = list(yin)
     r = list(rin)
     xbar = mean(x)
-    ybar = mean(y)
-    
     yadj = []
     xy = []
     xx = []
     n = len(x)
     computationTime = 0
-    #correlation = 0
-    #ssxx = []
-    #ssyy = []
-    #ssxy = []
+    correlation = 0
+    sst = []
+    ssr = []
     
-    
-    
-    #Regression Computation
-    
-    t0 = time.clock() #starts timer
-    
+    b0, a0, r0 ,p0, s0 = stats.linregress(x,y)
     for i in range(0,n): #creates y_adj
-        if (y[i] < ybar):
-            y[i] = y[i] + r[i]
+        if (y[i] < (a0 + b0*x[i])):
+            yadj.append(y[i] + r[i])
         else:
-            y[i] = y[i] - r[i]
+            yadj.append(y[i] - r[i])
     for i in range(0,n): #creates xy
-        xy.append(x[i]*y[i])
+        xy.append(x[i]*yadj[i])
     for i in range(0,n): #creates xx
-        xx.append(x[i]**2)
-        
+        xx.append(x[i]**2)    
     sumxx = sum(xx)
     sumxy = sum(xy)
-    ybar = mean(y)
-        
-    a = ((ybar*sumxx - xbar*sumxy)) / (sumxx-n*(xbar**2)) #finds a
-    b = (sumxy - n*xbar*ybar)/(sumxx-n*(xbar**2)) #finds b
+    yadjbar = mean(yadj)    
+    a = ((yadjbar*sumxx - xbar*sumxy)) / (sumxx-n*(xbar**2)) #finds a
+    b = (sumxy - n*xbar*yadjbar)/(sumxx-n*(xbar**2)) #finds b       
+    
+    for i in range(0,n): #creates ssxx
+        sst.append((yadj[i]-yadjbar)**2)
+    for i in range(0,n): #creates ssyy
+        ssr.append(((a+b*x[i])-yadjbar)**2)    
+    sst = sum(sst)
+    ssr = sum(ssr)
+    correlation = ssr/sst
     
     t1 = time.clock() #ends timer
-    
     computationTime = t1 - t0 #calculates time
     
+    if (plot == True):
+        plt.scatter(x,y,1,'y')
+        plt.scatter(x,yadj)
+        plt.axis([min(x)-1,max(x)+1,min(yadj)-1,max(yadj)+1])
+        axis = linspace(min(x),max(x))
+        reg = a + b*axis
+        plt.plot(axis,reg)
+        plt.show()
     
-    
-    # Regression Analysis         
-    
-    #for i in range(0,n): #creates ssxx
-    #    ssxx.append((x[i]-xbar)**2)
-    #for i in range(0,n): #creates ssyy
-    #    ssyy.append((yadj[i]-yadjbar)**2)
-    #for i in range(0,n): #creates ssxy
-    #    ssxy.append(((yadj[i]-yadjbar)**2)*((x[i]-xbar)**2)) 
-    #    
-    #ssxx = sum(ssxx)
-    #ssyy = sum(ssyy)
-    #ssxy = sum(ssxy)
-    #
-    #correlation = (ssxy**2)/(ssxx*ssyy)
-    
-    
-    
-    #Regression Display
-    
-    print 'x: ' + str(x)
-    print 'y: ' + str(y)
-    print 'r: ' + str(r)
-    print 'xbar: ' + str(xbar)
-    print 'ybar: ' + str(ybar)
-    
-    print "a: " + str(a)
-    print "b: " + str(b)
-    #print "Correlation: " + str(correlation)
-    print "ComputationTime: " + str(computationTime)
-    
-    area = []
-    
-    for i in range(0,n):
-        area.append(pi*((129*r[i])**2))
-    
-    plt.scatter(x,y,area, 'y')
-    axis = linspace(min(x),max(x),100)
-    reg = a + b*axis
-    plt.plot(axis,reg)
-    plt.show()
-  
+    print ""
+    print ""
+    print ""
+    print "-------------------------"
+    print ""
+    print "Linear Regression Results"
+    print ""
+    print "ComputationTime:   " + str(computationTime)
+    print "a0:                " + str(a0)
+    print "b0:                " + str(b0)
+    print "a:                 " + str(a)
+    print "b:                 " + str(b)
+    print "Correlation:       " + str(correlation)
+    print ""
+    print "-------------------------"
+    print ""
+    print ""
+    print ""
 
+x = [2,4,8,12]
+y = [2,4,4,4]
+r = [0.6,0.8,0.3,0.2]
 
+plot = False
+
+analysis(x,y,r,plot)
 
 #x = [12.0, 8.0, 4.0, 0.0]
 #y = [4.0, 2.0, 4.0, 2.0]
 #r = [1.2, 1, 0.8, 0.9]
 
-analysis([12.0, 8.0, 4.0, 0.0],[4.0, 2.0, 4.0, 2.0],[1.2, 1, 0.8, 0.9])
+#x = [1,5,9,13,17,21]
+#y = [2,2,6,6,6,10]
+#r = [0.2,1.4,1.5,0.2,0.9,1.9]
+
+#x = [-2,2,4,8,12,16]
+#y = [2,2,4,4,4,4]
+#r = [0.1,0.6,0.8,0.3,0.2,0.9]
